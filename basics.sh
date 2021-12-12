04ea99b89d7299798078817f2b02a507789097ec
tejas303525 <68140019+tejas303525@users.noreply.github.com>
2021-12-12 21:15:40 +0400

    Add files via upload

---
+++ basics.sh
@@ -0,0 +1,61 @@
+#!/usr/bin/bash
+sudo apt update
+echo "Updated"
+sleep 2
+sudo apt full-upgrade -y
+echo "Upgraded!"
+sleep 2
+sudo apt-get install ubuntu-restricted-extras
+echo "Installed Extras"
+sleep 2
+sudo apt install gnome-tweaks
+sleep 2
+sudo apt install preload
+sleep 2 
+sudo apt install timeshift
+sleep 2
+sudo apt install synaptic
+sleep 2
+sudo apt install gparted
+echo "Installed GPARTED SYNAPTIC TIMESHIFT PRELOAD AND GNOME TWEAKS"
+sleep 2
+sudo apt install tlp tlp-rdw
+sudo tlp start
+sleep 2
+sudo apt install flatpak
+sudo apt install gnome-software-plugin-flatpak
+sleep 2
+sudo apt install ufw
+sudo apt install gufw
+echo "Installed Firewall"
+sleep 2
+curl -s https://get.sdkman.io | bash
+echo "Installed SDKMAN"
+sleep 2
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.0/install.sh | bash
+nvm install --lts
+echo "Installed Node JS"
+sleep 2
+mkdir installers
+cd installers
+wget https://repo.anaconda.com/miniconda/Miniconda3-py38_4.10.3-Linux-x86_64.sh
+bash Miniconda3-py38_4.10.3-Linux-x86_64.sh
+echo "Installed Conda"
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+All creds goes to :-
+
+
+ _______  _______  _______ _________     _______           ______   _______  _______  _______  _______  _        _______  _       
+(  ____ \(  ___  )(  ___  )\__   __/    (  ____ \|\     /|(  __  \ (  ___  )(  ____ )(  ____ \(  ___  )( (    /|(  ___  )( (    /|
+| (    \/| (   ) || (   ) |   ) (       | (    \/| )   ( || (  \  )| (   ) || (    )|| (    \/| (   ) ||  \  ( || (   ) ||  \  ( |
+| (_____ | (___) || (___) |   | | _____ | (_____ | |   | || |   ) || (___) || (____)|| (_____ | (___) ||   \ | || (___) ||   \ | |
+(_____  )|  ___  ||  ___  |   | |(_____)(_____  )| |   | || |   | ||  ___  ||     __)(_____  )|  ___  || (\ \) ||  ___  || (\ \) |
+      ) || (   ) || (   ) |   | |             ) || |   | || |   ) || (   ) || (\ (         ) || (   ) || | \   || (   ) || | \   |
+/\____) || )   ( || )   ( |___) (___    /\____) || (___) || (__/  )| )   ( || ) \ \__/\____) || )   ( || )  \  || )   ( || )  \  |
+\_______)|/     \||/     \|\_______/    \_______)(_______)(______/ |/     \||/   \__/\_______)|/     \||/    )_)|/     \||/    )_)
+                                                                                                                                  
+
+https://github.com/saai-sudarsanan-d
+
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
